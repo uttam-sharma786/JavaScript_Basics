f35605d97fd659002c700e127cde954a28ac8141
uttam-sharma786 <uttamsharma.8560@gmail.com>
2024-08-05 16:19:48 +0530

    firstclass

---
+++ 01_Basics/Datatypes.js
@@ -0,0 +1,75 @@
+"use strict" // treat all JS code as newer version (like class, arrow functions)
+
+// alert("Hello") // we are using nodejs not browser
+
+// console.log("Hello")
+console.log(typeof 2+3);
+
+let name = "uttam" // string
+console.log(typeof name);
+let age = 13;
+console.log(typeof age)
+age = "13"
+console.log(typeof age);
+let isLoggidIn =" "
+console.log(isLoggidIn)
+
+
+console.log(isLoggidIn);
+
+
+// number => 2 to pwer53
+// bignet
+// String => ""
+// boolean => True/false
+// null => standalone value
+// undefined;
+
+let score;
+console.log(typeof score);
+let scores = null;
+console.log(typeof scores);
+
+
+/*
+Data type conversion 
+*/
+
+
+age = "28787"
+
+let valueInNumber = Number(age);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let someNumber = 23;
+let changeString = String(someNumber)
+console.log(typeof changeString)
+
+
+
+//=================================== opertions ==================================//
+
+// let value = 4
+// let negvalue = -value
+// console.log(negvalue);
+
+
+
+
+// console.log("1" +2);
+
+// console.log("1" + "2");
+// console.log("1"+"2"+3);
+// console.log(1+2+"2");
+
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+let x = 345
+let y = ++x
+console.log(x)
+
+
---
+++ 01_Basics/variables.js
@@ -0,0 +1,25 @@
+const accountId = 1334
+console.log(accountId)
+let accountEmail = "uttamsharma@google.com";
+console.log(accountEmail);
+
+var accountPassword = "23435345";
+console.log(accountPassword);
+
+
+accountCity = "Jaipiur"
+accountCity = "Delhi"
+// console.log(accountCity);
+
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountCity]);
+
+
+// accountId = 1232;
+// accountEmail="tr@wraptron.com"
+// accountPassword="14453"
+// console.log(accountPassword)
+
+
+
