10866072c8fcbc501c2813b060650310c46e0445
uttam-sharma786 <uttamsharma.8560@gmail.com>
2024-08-07 16:11:06 +0530

    Number and Math

---
+++ 01_Basics/Number.js
@@ -0,0 +1,35 @@
+// const score = 400.566
+// console.log( score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(typeof (balance.toString()));
+// console.log(score.toFixed(2));
+
+// console.log(score.toPrecision(3))
+
+
+//######################################## Math #########################################
+
+// console.log(Math);
+// console.log(Math.abs(4.5))
+const mathValue = 4.534534254
+// console.log(Math.ceil(mathValue))
+// console.log(Math.floor(mathValue))
+
+
+// console.log(Math.sqrt(mathValue))
+// console.log(Math.pow(4,3))
+
+// console.log(Math.round(mathValue))
+// console.log(Math.min(3,45,6,7));
+// console.log(Math.max(45,6,7,29))
+
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+
+
+
+
--- 01_Basics/string.js
+++ 01_Basics/string.js
@@ -1,9 +1,9 @@
 const name1 = "Uttam";
 const repoCount = 10
 
-console.log(name1+repoCount+"value")
+// console.log(name1+repoCount+"value")
 
-console.log(`Hello my name is ${name1} my repo count is ${repoCount} repo count`);
+// console.log(`Hello my name is ${name1} my repo count is ${repoCount} repo count`);
 
 // string declaration
 const gameName = new String("Uttam-ut")
@@ -11,8 +11,21 @@ const gameName = new String("Uttam-ut")
 // string method
 
 console.log(gameName[0]);
-console.log(gameName.__proto__);
 
-console.log(gameName.length);
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf());
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4);
+console.log(newString)
+// console.log(gameName.includes('a'))
+// console.log(gameName.slice(1,4))
+console.log(gameName.split(4))
+const anotherString = " Uttam  "
+console.log(anotherString.trim());
+
+const url = "https://uttam.com/uttam%20sharma"
+console.log(url.replace('%20','-----'))
